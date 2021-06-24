d541de779b29256446d4823d48b16436073b4f28
WubzyGD <wubzygd@gmail.com>
2021-06-23 18:06:27 -0600

    more emotes

--- commands/fun/neko.js
+++ commands/fun/neko.js
@@ -4,27 +4,26 @@ const UserData = require('../../models/user');
 const makeId = require('../../util/makeid');
 
 module.exports = {
-    name: "smile",
-    aliases: ['grin'],
-    help: "Spread the happiness with {{p}}smile!",
+    name: "neko",
+    help: "Get a random neko image",
     meta: {
         category: 'Social',
-        description: "Show everyone just how happy you are!",
-        syntax: '`smile`',
+        description: "Get a random neko image",
+        syntax: '`neko`',
         extra: null
     },
     async execute(message, msg, args, cmd, prefix, mention, client) {
-        let savess = await Saves.findOne({name: 'smile'}) ? await Saves.findOne({name: 'smile'}) : new Saves({name: 'smile'});
+        let savess = await Saves.findOne({name: 'neko'}) ? await Saves.findOne({name: 'neko'}) : new Saves({name: 'neko'});
         let saves = savess.saves;
         if (!args.length) {return message.channel.send(new Discord.MessageEmbed()
-            .setTitle(`${message.guild ? message.member.displayName : message.author.username} has a big smile on their face!`)
+            .setTitle(`Here's a random neko for you :3`)
             .setImage(String(Array.from(saves.values())[Math.floor(Math.random() * saves.size)]))
-            .setColor('00cd00')
+            .setColor('aa459d')
         );}
         if (['s', 'save', 'n', 'new', 'a', 'add'].includes(args[0].toLowerCase())) {
             if (!args[1]) {return message.channel.send('oi there cunt, give me a link of an image to add!');}
             let tu = await UserData.findOne({uid: message.author.id});
-            if ((!tu || !tu.developer) && !client.misc.savers.includes(message.author.id)) {return message.reply("You must be a Luno Developer in order to add new smile GIFs.");}
+            if ((!tu || !tu.developer) && !client.misc.savers.includes(message.author.id)) {return message.reply("You must be a Luno Developer in order to add new neko GIFs/images.");}
             let e = true;
             let id;
             while (e === true) {id = makeId(6); if (!saves.has(id)) {e = false;}}
